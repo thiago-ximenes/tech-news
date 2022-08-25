644c049c129c0b49490eb967a0aacb63020568de
Thiago Ximenes <thiagoximeneslima@gmail.com>
2022-08-24 22:37:05 -0300

    pass: requisito 4

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,7 @@
 import requests
 import parsel
 from time import sleep
+import re
 
 
 # Requisito 1
@@ -27,16 +28,39 @@ def scrape_novidades(html_content):
 # Requisito 3
 def scrape_next_page_link(html_content):
     selector = parsel.Selector(text=html_content)
-    link = selector.css(
-        "div.nav-links a.next.page-numbers::attr(href)"
-    ).get()
+    link = selector.css("div.nav-links a.next.page-numbers::attr(href)").get()
     print(link)
     return link
 
 
 # Requisito 4
 def scrape_noticia(html_content):
-    pass
+    selector = parsel.Selector(text=html_content)
+    url = selector.css("link[rel='canonical']::attr(href)").get()
+    title = selector.css("h1.entry-title::text").get()
+    timestamp = selector.css("ul.post-meta li.meta-date::text").get()
+    writer = selector.css("ul.post-meta span.author a::text").get()
+    select_comments = selector.css("#comments").get()
+    comments_count = "".join(
+        filter(lambda i: i.isdigit(), select_comments)
+    ) if select_comments != None else 0
+    summary = selector.css("div.entry-content p:first-of-type").get()
+    tags = selector.css("ul li a[rel='tag']::text").getall()
+    category = selector.css("div.meta-category span.label::text").get()
+
+    news_dict = {
+        "url": url,
+        "title": title.replace('\xa0', '').strip(),
+        "timestamp": timestamp,
+        "writer": writer,
+        "comments_count": comments_count,
+        "summary": re.sub(re.compile('<.*?>'), '', summary).replace('\xa0', '').strip(),
+        "tags": tags,
+        "category": category,
+    }
+
+    return news_dict
+
 
 # Requisito 5
 def get_tech_news(amount):
