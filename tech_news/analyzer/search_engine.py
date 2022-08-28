65f4fe7ab32d6511be0855ca5dab0ddf4f066a67
Thiago Ximenes <thiagoximeneslima@gmail.com>
2022-08-28 15:04:27 -0300

    refactor: abstrai função para serializar returno do banco e passa no requisito 7

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,4 +1,9 @@
 from tech_news import database
+from datetime import datetime
+
+
+def serialize_news(news):
+    return [(new["title"], new["url"]) for new in news]
 
 
 # Requisito 6
@@ -11,14 +16,27 @@ def search_by_title(title):
     }
     search_news = list(database.db.news.find(query, options))
 
-    serialized_news = [(new["title"], new["url"]) for new in search_news]
-
-    return serialized_news
+    return serialize_news(search_news)
 
 
 # Requisito 7
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    try:
+        formatted_date = datetime.strptime(date, "%Y-%m-%d").strftime(
+            "%d/%m/%Y"
+        )
+    except ValueError:
+        raise ValueError("Data inválida")
+
+    query = {"timestamp": {"$regex": formatted_date, "$options": "i"}}
+    options = {
+        "title": 1,
+        "url": 1,
+        "_id": 0,
+    }
+    search_news = list(database.db.news.find(query, options))
+
+    return serialize_news(search_news)
 
 
 # Requisito 8
